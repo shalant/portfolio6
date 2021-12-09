ec6ceaae9516db7bd948e38a2ebdce2e74787248
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-12-09 11:02:33 -0600

    working on about

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,17 +10,17 @@ function About() {
             <img src={headshot} alt='me' className='headshot' />
             <p className='about-text'>Full Stack Junior Web Developer with a background in Music and BS in Chemistry, 20 years performing/ teaching music. Passionate about coding with MERN stack and working with teammates. Strong independent learner that enjoys pithy challenges. Looking forward to my new career creating value for a company.</p>
             <div className='fontawesome-icons'>
-                <i class="fab fa-html5 fa-3x"></i>
-                <i class="fab fa-css3-alt fa-3x"></i>
-                <i class="fab fa-js-square fa-3x"></i>
-                <i class="fab fa-react fa-3x"></i>
-                <i class="fab fa-node fa-3x"></i>
-                <i class="fas fa-database fa-3x"></i>
-                <i class="fab fa-npm fa-3x"></i>
-                <i class="fab fa-bootstrap fa-3x"></i>
-                <i class="fas fa-passport fa-3x"></i>
+                <i class="fab fa-html5 fa-4x"></i>
+                <i class="fab fa-css3-alt fa-4x"></i>
+                <i class="fab fa-js-square fa-4x"></i>
+                <i class="fab fa-react fa-4x"></i>
+                <i class="fab fa-node fa-4x"></i>
+                <i class="fas fa-database fa-4x"></i>
+                <i class="fab fa-npm fa-4x"></i>
+                <i class="fab fa-bootstrap fa-4x"></i>
+                <i class="fas fa-passport fa-4x"></i>
             </div>
-            <IconContext.Provider value={{ size: '50px', margin: '0 2px 0 px' }}>
+            <IconContext.Provider value={{ size: '50px', margin: '20px 20px 5px' }}>
                 <SiRedux />
                 <SiMysql />
                 <SiMongodb />
