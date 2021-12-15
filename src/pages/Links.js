fb304791c16a1bf0926904de161d03cb0ec42b1e
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-12-15 15:55:21 -0600

    removed external link from links page

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -8,24 +8,24 @@ function Links () {
                 <div class="row gx-5">
                     <div class="col">
                         <div class="links__link p-3 border bg-light">
-                            <a href="https://dougrosenberg.com" target="_blank">Music page</a>
+                            <a href="https://dougrosenberg.com">Music page</a>
                         </div>
                         <div class="links__link p-3 border bg-light">
-                            <a href="https://dougrosenberg.com" target="_blank">Something Cool</a>
+                            <a href="https://dougrosenberg.com">Something Cool</a>
                         </div>
                         <div class="links__link p-3 border bg-light">
-                            <a href="https://dougrosenberg.com" target="_blank">Something Cool</a>
+                            <a href="https://dougrosenberg.com">Something Cool</a>
                         </div>
                     </div>
                     <div class="col">
                         <div class="links__link p-3 border bg-light">
-                            <a href="https://gpt3.dougrosenbergdev.com" target="_blank">GPT3 Project</a>
+                            <a href="https://gpt3.dougrosenbergdev.com">GPT3 Project</a>
                         </div>
                         <div class="links__link p-3 border bg-light">
-                            <a href="https://gpt3.dougrosenbergdev.com" target="_blank">Something Cool</a>
+                            <a href="https://gpt3.dougrosenbergdev.com">Something Cool</a>
                         </div>
                         <div class="links__link p-3 border bg-light">
-                            <a href="https://gpt3.dougrosenbergdev.com" target="_blank">Something Cool</a>
+                            <a href="https://gpt3.dougrosenbergdev.com">Something Cool</a>
                         </div>
                     </div>
                 </div>
