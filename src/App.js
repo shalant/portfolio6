0ad0c86e3ab77e83d904c590058826e8a11fee23
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-12-15 21:59:42 -0600

    fucking around with weather widget, potentially adding more cleanup later...

--- src/App.js
+++ src/App.js
@@ -13,14 +13,33 @@ import Links from './pages/Links';
 import Cards from './components/Cards';
 import { Navbar, Nav } from 'react-bootstrap';
 import ContactGlove from './pages/ContactGlove';
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
 
 
 function App() {
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: 'YOUR-API-KEY',
+    lat: '48.137154',
+    lon: '11.576124',
+    lang: 'en',
+    unit: 'metric', // values are (metric, standard, imperial)
+  });
   return (
     <Router>
       <Container fluid>
         <NavbarD />
-        
+        <ReactWeather 
+          isLoading={isLoading}
+          errorMessage={errorMessage}
+          data={data}
+          lang="en"
+          locationLabel="Munich"
+          unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+          showForecast
+          theme={customStyles}
+
+        />
+
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
@@ -35,5 +54,25 @@ function App() {
     </Router>
   );
 }
+
+const customStyles = {
+	fontFamily:  'Helvetica, sans-serif',
+	gradientStart:  '#0181C2',
+	gradientMid:  '#04A7F9',
+	gradientEnd:  '#4BC4F7',
+	locationFontColor:  '#FFF',
+	todayTempFontColor:  '#FFF',
+	todayDateFontColor:  '#B5DEF4',
+	todayRangeFontColor:  '#B5DEF4',
+	todayDescFontColor:  '#B5DEF4',
+	todayInfoFontColor:  '#B5DEF4',
+	todayIconColor:  '#FFF',
+	forecastBackgroundColor:  '#FFF',
+	forecastSeparatorColor:  '#DDD',
+	forecastDateColor:  '#777',
+	forecastDescColor:  '#777',
+	forecastRangeColor:  '#777',
+	forecastIconColor:  '#4BC4F7',
+};
 
 export default App;
