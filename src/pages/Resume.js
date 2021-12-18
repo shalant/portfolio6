d2e703ef43d5c1bb589c4ee62fe16c1e38072f1f
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-12-17 22:13:48 -0600

    actually updated resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './resume.css';
-import resume from '../assets/images/DouglasRosenbergResume.png'
+import resume from '../assets/images/DouglasRosenbergResumeDec2021.png'
 
 function Resume() {
     return (
         <div className='resume__container'>
             <div className='resume-link'>
-                <a href='https://docs.google.com/document/d/1G1z3cVN6IjHW4M2zVytsoE6g_-fWETb0qTSSZqI05Tk/edit?usp=sharing'>Resume</a>
+                <a href='https://drive.google.com/file/d/1nVuDMgo_U-KQoDzbxjR4AbpHF87_Zqbv/view?usp=sharing'>Resume</a>
             </div>
             <div className='resume-div'>
                 <img src={resume} alt='resume' className='resume-img'></img>
