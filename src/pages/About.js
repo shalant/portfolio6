c371bc7d8991df3b44ed84c34f5f777669a5c7fc
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-05-15 13:30:50 -0500

    added more software icons

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './about.css';
-import { SiMysql, SiRedux, SiMongodb, SiJest, SiMicrosoftword, SiMicrosoftexcel, SiMaterialui } from 'react-icons/si';
+import { SiMysql, SiRedux, SiMongodb, SiJest, SiMicrosoftword, SiMicrosoftexcel, SiMaterialui, SiPython, SiAmazonaws, SiDocker, SiKubernetes, SiWordpress, SiAnsible, SiJenkins, SiTerraform } from 'react-icons/si';
 import { IconContext } from "react-icons";
 import headshot from '../assets/images/headshot.jpg';
 
@@ -9,7 +9,7 @@ function About() {
         <div className='about'>
             <h1 className='about__header'>About Doug Rosenberg Dev</h1>
             <img src={headshot} alt='me' className='headshot' />
-            <p className='about-text'>Full Stack Junior Web Developer with a background in Music and BS in Chemistry, 20 years performing/ teaching music. Passionate about coding with MERN stack and working with teammates. Strong independent learner that enjoys pithy challenges. Looking forward to my new career creating value for a company.</p>
+            <p className='about-text'>AWS-certified Cloud Practioner/MERN stack developer with a background in Music and BS in Chemistry. 20 years performing/ teaching music. Passionate about Cloud Computing, React, and working with a good team. Strong independent learner that enjoys pithy challenges. Looking forward to my new career creating value for a company.</p>
             <div className='icons'>
                 <div className='fontawesome-icons'>
                     <i class="fab fa-html5 fa-4x"></i>
@@ -21,6 +21,8 @@ function About() {
                     <i class="fab fa-npm fa-4x" icons-wrap></i>
                     <i class="fab fa-bootstrap fa-4x icons-wrap"></i>
                     <i class="fas fa-passport fa-4x icons-wrap"></i>
+                    <i class="fab fa-java fa-4x icons-wrap"></i>
+                    <i class="fab fa-python fa-4x icons-wrap"></i>
                 </div>
                 <div className='react-icons'>
                     <IconContext.Provider value={{ size: '60px', margin: '50px 20px 5px', padding: '10px' }}>
@@ -31,6 +33,14 @@ function About() {
                         <SiMicrosoftword className='react-icons' />
                         <SiMicrosoftexcel className='react-icons' />
                         <SiMaterialui className='react-icons' />
+                        <SiAmazonaws className='react-icons' />
+                        <SiDocker className='react-icons' />
+                        <SiKubernetes className='react-icons' />
+                        <SiWordpress className='react-icons' />
+                        <SiAnsible className='react-icons' />
+                        <SiJenkins className='react-icons' />
+                        <SiTerraform className='react-icons' />
+
                     </IconContext.Provider>
                 </div>
             </div>
